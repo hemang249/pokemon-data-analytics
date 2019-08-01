ea5c42744fef183b66c90bd088d13e28a66be8f6
Hemang Bhogayata <hemang249@gmail.com>
2019-08-01 23:25:28 +0530

    Code CleanUp

--- height-analytics.py
+++ height-analytics.py
@@ -9,27 +9,27 @@ parsed_data = json.loads(data)
 
 results = parsed_data['results']
 names = []
-jsons = []
+pokemon_json = []
 pokemons  = []
 
 i = 0
-while i < 100:
+while i < 10:
     names.append(results[i]['name'])
-    jsons.append(results[i]['url'])
+    pokemon_json.append(results[i]['url'])
     i = i + 1
 
-for url in jsons:
+for url in pokemon_json:
     response = requests.get(url)
     pokemons.append(json.loads(response.text))
 
-heights = []
+pokemon_heights = []
+
 
 for pokemon in pokemons:
     pokemon_data = []
     pokemon_data.append(pokemon['height'])
-    pokemon_data.append(pokemon['name'])
-    heights.append(pokemon_data)
+    pokemon_data.append(pokemon['types'][0]['type']['name'])
+    pokemon_heights.append(pokemon_data)
 
-heights.sort()
-
-print(heights)
+pokemon_heights.sort()
+print(pokemon_heights)
